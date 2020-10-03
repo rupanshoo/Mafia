074281ec367c086669922c5c5ce910f56a671364
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-03 15:47:32 +0530

    classes

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+abstract class Character{
+
+}
+
+class Mafia extends Character{
+
+}
+
+class Detective extends Character{
+
+}
+
+class Healer extends Character{
+
+}
+
+class Commoner extends Character{
+
+}
+
+public class Main {
+
+}
+
