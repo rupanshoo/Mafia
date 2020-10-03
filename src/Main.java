3611438d2a73b4308e0f85617edeada6063038ec
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-03 15:54:31 +0530

    classes

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,36 @@
 import java.util.*;
 
-abstract class Character{
 
+abstract class Character{
+    private int HP;
 }
+
 
 class Mafia extends Character{
 
 }
 
+
 class Detective extends Character{
 
 }
+
 
 class Healer extends Character{
 
 }
 
+
 class Commoner extends Character{
 
 }
 
+
 public class Main {
+
+    public static void main(String args[]){
+
+    }
 
 }
 
